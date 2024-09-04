00a8e8fdfa70bf0d51802d2b5a4d814ee3157ac9
fiv <1647715359@qq.com>
2024-09-04 19:28:42 +0800

    read

---
+++ sglang/srt/layers/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# @FileName : __init__.py.py
+# @Time : 2024/9/4 14:41
+# @Author : fiv
---
+++ tmp.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @FileName : tmp.py
+# @Time : 2024/9/4 15:17
+# @Author : fiv
+
+
+import sglang as sgl
+
+sgl.set_default_backend(runtime)
+@sgl.function
+def test():
+    print('test')
